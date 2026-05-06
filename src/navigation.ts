67df21cb96fefb95cfdc94a71b51b503746b9fa1
Samuel Gonzalez Ruiz <sgruiz462@gmail.com>
2026-05-06 09:30:48 +0200

    Initial commit SAF-EAGLE

---
+++ src/navigation.ts
@@ -0,0 +1,70 @@
+import { getPermalink, getAsset } from './utils/permalinks';
+
+export const headerData = {
+  links: [
+    {
+      text: 'Home',
+      href: getPermalink('/'),
+    },
+    {
+      text: 'Research',
+      links: [
+        {
+          text: 'HYDRO-SAF',
+          href: getPermalink('/hydro-saf'),
+        },
+        {
+          text: 'SAF-FALCON',
+          href: 'https://blogs.ujaen.es/tep250/?page_id=100',
+        },
+        {
+          text: 'METHOD-EAGLE',
+          href: getPermalink('/method-eagle'),
+        },
+      ],
+    },
+    {
+      text: 'Facilities',
+      href: getPermalink('/facilities'),
+    },
+    {
+      text: 'Publications',
+      href: getPermalink('/publications'),
+    },
+    {
+      text: 'Contact',
+      href: getPermalink('/contact'),
+    },
+  ],
+  actions: [],
+};
+
+export const footerData = {
+  links: [
+    {
+      title: 'Research',
+      links: [
+        { text: 'HYDRO-SAF', href: getPermalink('/hydro-saf') },
+        { text: 'SAF-FALCON', href: 'https://blogs.ujaen.es/tep250/?page_id=100' },
+        { text: 'METHOD-EAGLE', href: getPermalink('/method-eagle') },
+      ],
+    },
+    {
+      title: 'Project',
+      links: [
+        { text: 'Facilities', href: getPermalink('/facilities') },
+        { text: 'Publications', href: getPermalink('/publications') },
+        { text: 'Contact', href: getPermalink('/contact') },
+      ],
+    },
+  ],
+  secondaryLinks: [
+   
+  ],
+  socialLinks: [
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com' },
+  ],
+  footNote: `
+    SAF-EAGLE Project · University of Castilla-La Mancha (UCLM)
+  `,
+};
